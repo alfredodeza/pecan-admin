909fe14f3b0a0dbb9f9772892b2305107ead4c2d
Alfredo Deza <alfredodeza@gmail.com>
2013-07-19 20:26:46 -0400

    we really do not need form validation here

--- pecan_admin/controllers/root.py
+++ pecan_admin/controllers/root.py
@@ -1,8 +1,6 @@
 from pecan import expose, request, redirect, secure
-from pecan.ext.wtforms import with_form
 from pecan_admin import models
 from pecan_admin.lib.auth import save_user_session, remove_user_session
-from pecan_admin.forms.login import LoginForm
 
 from pecan_admin.controllers.users import UsersController
 
@@ -17,18 +15,12 @@ class RootController(object):
         return dict()
 
     @index.when(method='POST', template='index.html')
-    @with_form(LoginForm)
     def _post_login(self, *args, **kwargs):
-        # XXX doing it wrong
-        form = request.pecan.get('form')
-        if form:
-            if getattr(form, 'errors'):
-                request.context['errors'] = form.errors
-                return dict(form_data=form.data, errors=form.errors)
-
         user = models.User.filter_by(username=kwargs['username']).first()
-        save_user_session(user)
-        redirect('/')
+        if user.validate_password(kw.get('password')):
+            save_user_session(user)
+            redirect('/')
+        return dict(errors='wrong password or username')
 
     @expose(template='signin.html')
     def signin(self):
