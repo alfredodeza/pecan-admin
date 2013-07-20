132d61f3ee17e2d67f68dd93d8896d9fdb5dc8b4
Alfredo Deza <alfredodeza@gmail.com>
2013-07-20 11:39:30 -0400

    update requirements to actually work

--- setup.py
+++ setup.py
@@ -8,7 +8,12 @@ setup(
     description = 'An admin interface for the Pecan framework',
     author = 'Alfredo Deza',
     author_email = 'alfredo [at] deza.pe',
-    install_requires = ["pecan"],
+    install_requires = [
+        'pecan', 
+        'sqlalchemy<=0.9', 
+        'Beaker==1.6.4',
+        'WebHelpers==1.3',
+        ],
     zip_safe = False,
     include_package_data = True,
     packages = find_packages(exclude=['ez_setup']),
