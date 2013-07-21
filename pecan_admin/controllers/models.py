bea6de4336bd380b2009e97fd610d5d485121f0a
Alfredo Deza <alfredodeza@gmail.com>
2013-07-21 15:07:49 -0400

    create a single model controller

--- pecan_admin/controllers/models.py
+++ pecan_admin/controllers/models.py
@@ -1,7 +1,20 @@
 from pecan import expose
 
-
 class ModelController(object):
+
+    def __init__(self, admin_model, model):
+        self.admin_model = admin_model
+        self.model = model
+
+    @expose(template='models/edit.html')
+    def index(self):
+        return dict(
+                name=self.admin_model.name,
+                model=self.model,
+                fields=self.model.as_dict().keys())  # XXX come on now, so lazy
+
+
+class ModelsController(object):
 
     def __init__(self, admin_model):
         self.admin_model = admin_model
@@ -11,6 +24,12 @@ class ModelController(object):
     def index(self):
         all_items = self.model.query.all()  # oh so very dangerous
         return dict(
-                name=self.admin_model.name, 
-                items=all_items, 
+                name=self.admin_model.name,
+                items=all_items,
                 fields=self.admin_model.fields)
+
+    @expose()
+    def _lookup(self, _id, *remainder):
+        model = self.model.get(int(_id))
+        if model:
+            return ModelController(self.admin_model, model), remainder
